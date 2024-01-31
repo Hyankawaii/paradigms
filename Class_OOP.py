0b389b89050d3c6ab788fcbaf04328da2d7aba56
Hyankawaii <125204317+Hyankawaii@users.noreply.github.com>
2024-01-31 09:12:51 +0700

    test

--- Class_OOP.py
+++ Class_OOP.py
@@ -1,6 +1,8 @@
 class dog:
-    def __innit__(self, name, age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
     def sit():
         print("{self.name} is sitting")
+
+
